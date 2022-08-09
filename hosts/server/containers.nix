1f892a278f0c2eac03a2b56c83f2a3229fbfd791
sine <liu.siyao@qq.com>
2022-08-10 03:17:13 +0800

    chore: update kiwi

--- hosts/server/containers.nix
+++ hosts/server/containers.nix
@@ -82,7 +82,7 @@ in
 
     kiwi = {
       autoStart = true;
-      image = "sineliu/kiwi:0.8.16";
+      image = "sineliu/kiwi:0.8.17";
       ports = [ "${config.kiwi.port}:8080" ];
       volumes = [ "${config.kiwi.folder}:/data" ];
       user = "1000:100";

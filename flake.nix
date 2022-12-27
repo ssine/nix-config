824f2d81ad6b27dfecfe0ba17b87488f6fb6d2cf
sine <liu.siyao@qq.com>
2022-12-28 02:25:37 +0800

    chore: update

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "An example NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.05";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-cn = {
@@ -30,7 +30,7 @@
       code01 = import ./hosts/code01 inputs;
     };
     homeConfigurations = {
-      devbox = import ./hosts/bytedance-devbox inputs;
+      # devbox = import ./hosts/bytedance-devbox inputs;
       wsl = import ./hosts/wsl2 inputs;
     };
     nixopsConfigurations.default = {

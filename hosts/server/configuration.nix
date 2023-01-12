b0efa5a878dc78fff9f70a0484232f42fa04f1d7
sine <liu.siyao@qq.com>
2023-01-13 03:25:16 +0800

    feat: add tailscale

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -301,6 +301,7 @@ in
   environment.systemPackages = (import ../../modules/common).servertools pkgs ++ (with pkgs; [
     ossutil
     postgresql_14
+    tailscale
 
     (python3.withPackages (p: with p; [
       requests
@@ -319,6 +320,36 @@ in
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+
+  services.tailscale.enable = true;
+
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey ${configs.tailscale.key}
+    '';
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
